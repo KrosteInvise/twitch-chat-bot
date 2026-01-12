d2c9b3d7fbde1a783bb60230b510bb21ed5977ae
Evgeniy <32774299+KrosteInvise@users.noreply.github.com>
2026-01-12 21:50:51 +0700

    Restore secrets.cs

---
+++ Assets/Scripts/ChatBot/Secrets.cs
@@ -0,0 +1,7 @@
+public static class Secrets
+{
+    public static string client_id = "";
+    public static string client_secret = "";
+    public static string bot_access_token = "";
+    public static string bot_refresh_token = "";
+}
